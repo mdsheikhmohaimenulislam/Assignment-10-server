cfa04bb96e9c05fca1838b8daa450dfe617c0d23
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-05-20 14:51:37 +0600

    added Middleware section on js file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send("simply GreenNest Server running")
+})
+
+
+app.listen(port, () => {
+    console.log(`Simply GreenNest server running on ${port}`);
+})
